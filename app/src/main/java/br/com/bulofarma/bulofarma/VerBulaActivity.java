4f03e1f84daf0bd122a4a471d2d0d7d59a927965
João Victor <jvictorco94@hotmail.com>
2016-05-12 17:39:27 -0300

    Layout + Imagens Layout Redefinido (Ainda irá sofrer mais alterações); Imagens adicionadas; Transição de Tela para Listar Bulas;

--- app/src/main/java/br/com/bulofarma/bulofarma/VerBulaActivity.java
+++ app/src/main/java/br/com/bulofarma/bulofarma/VerBulaActivity.java
@@ -2,14 +2,20 @@ package br.com.bulofarma.bulofarma;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 /**
  * Created by Victor Hugo on 11/05/2016.
  */
 public class VerBulaActivity extends Activity {
+    private ListView lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.ver_bula_activity);
+
+        lista = (ListView) findViewById(R.id.lista_bula_user);
+
+        registerForContextMenu(lista);
     }
 }
